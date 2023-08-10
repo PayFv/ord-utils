06272c422b168f7e5cb793c2e0f06b13d1128fde
Five <five@0x11.email>
2023-08-11 00:29:01 +0800

    test code

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { OrdTransaction, InscribeTransaction, UnspentOutput, AddressType } from 
 import { OrdUnspendOutput, UTXO_DUST } from "./OrdUnspendOutput";
 import { satoshisToAmount } from "./utils";
 import { LocalWallet, NetworkType, toPsbtNetwork, randomWIF, toXOnly } from "./LocalWallet";
-import { Psbt, payments, networks } from "bitcoinjs-lib";
+import { Psbt, payments, networks, Transaction } from "bitcoinjs-lib";
 import { off } from "process";
 
 export {
@@ -498,26 +498,26 @@ export async function createInscriptionRevealTx({
 
   tmp_reveal_tx.setInscription(tag, content_type, content)
 
-  tmp_reveal_tx.build_reveal_input( commit_tx )
+  tmp_reveal_tx.build_reveal_input(commit_tx)
 
   tmp_reveal_tx.addOutput(toAddress, ord_value)
- 
-  const reveal_tx: Psbt = await tmp_reveal_tx.createSignedInscribe() 
+
+  const reveal_tx: Psbt = await tmp_reveal_tx.createSignedInscribe()
 
   if (dump) {
     console.log(`createInscribe(reveal tx)==> `)
     tmp_reveal_tx.dumpTx(reveal_tx)
   }
 
-  let network_fee = 0 
-  if( cal_network_fee ) {
+  let network_fee = 0
+  if (cal_network_fee) {
     network_fee = await tmp_reveal_tx.calNetworkFee()
   }
 
-  const commit_p2tr =  tmp_reveal_tx.getP2TRAddress()
+  const commit_p2tr = tmp_reveal_tx.getP2TRAddress()
 
   return {
-    reveal_tx, 
+    reveal_tx,
     ord_value,
     network_fee,
     commit_p2tr,
@@ -644,18 +644,41 @@ export async function createInscriptionOffer({
   })
 
   let fixed_p2tr = payments.p2tr({
-    internalPubkey: toXOnly( Buffer.from('021bc91251f239f888706817e93e419f1532be84a1fc77166526902a36f6e8c707','hex')),
+    internalPubkey: toXOnly(Buffer.from('021bc91251f239f888706817e93e419f1532be84a1fc77166526902a36f6e8c707', 'hex')),
     network
   })
 
   const rec_p2tr = payments.p2tr({
-    internalPubkey: toXOnly(Buffer.from( rec_public_key, 'hex')),
+    internalPubkey: toXOnly(Buffer.from(rec_public_key, 'hex')),
     network
   })
 
   const inscription_p2tr = payments.p2tr({
-    internalPubkey: toXOnly(Buffer.from( inscription_utxo.scriptPk, 'hex')),
+    internalPubkey: toXOnly(Buffer.from(inscription_utxo.scriptPk, 'hex')),
     network
+  })
+
+  // fixed
+  offer.addInput({
+    hash: '0000000000000000000000000000000000000000000000000000000000000000',
+    index: 1,
+    witnessUtxo: {
+      value: amount,
+      script: fixed_p2tr.output
+    },
+    tapInternalKey: fixed_p2tr.internalPubkey
+  })
+
+
+  // fixed
+  offer.addInput({
+    hash: '0000000000000000000000000000000000000000000000000000000000000001',
+    index: 2,
+    witnessUtxo: {
+      value: amount,
+      script: fixed_p2tr.output
+    },
+    tapInternalKey: fixed_p2tr.internalPubkey
   })
 
   offer.addInput({
@@ -666,18 +689,18 @@ export async function createInscriptionOffer({
       script: inscription_p2tr.output
     },
     tapInternalKey: inscription_p2tr.internalPubkey,
-    sighashType: 0x03 | 0x80,
+    sighashType: Transaction.SIGHASH_SINGLE | Transaction.SIGHASH_ANYONECANPAY,
   })
 
-  // fixed
-  offer.addInput({
-    hash: '48500ece3e1b7b99ec0926f98678c14fd6eca4f6bc07284334c1b9319ef232da', 
-    index: 1,
-    witnessUtxo: {
-        value: amount,
-        script: fixed_p2tr.output
-    },
-    tapInternalKey: fixed_p2tr.internalPubkey
+
+  offer.addOutput({
+    address: fixed_p2tr.address,
+    value: 0
+  })
+
+  offer.addOutput({
+    address: fixed_p2tr.address,
+    value: 0
   })
 
   offer.addOutput({
@@ -690,8 +713,8 @@ export async function createInscriptionOffer({
     value: amount
   })
 
-  if( commission ) {
-    offer.addOutput( commission )
+  if (commission) {
+    offer.addOutput(commission)
   }
 
   return offer
@@ -699,43 +722,134 @@ export async function createInscriptionOffer({
 }
 
 export async function createUnsignedBuyOffer({
-  unsigned_psbt_hex,
+  order_psbt_hex,
   network,
+  dummy_utxos,
   utxos,
-  amount,
-  indexes,
-}:{
-  unsigned_psbt_hex: string;
+  inscription_output,
+  fee_rate,
+}: {
+  order_psbt_hex: string;
   network: any;
+  dummy_utxos: UnspentOutput[];
   utxos: UnspentOutput[];
-  amount: number;
-  indexes: number[];
+  inscription_output: string;
+  fee_rate: number;
 }) {
-  const offer = Psbt.fromHex( unsigned_psbt_hex , {
+  const offer = Psbt.fromHex(order_psbt_hex, {
     network
   })
 
-  let dummy_utxo 
-  let new_utxos = []
-  for( const utxo of utxos ) {
-    if( !dummy_utxo ) {
-      dummy_utxo = utxo
-    } else {
-      if( dummy_utxo.satoshis >= utxo.satoshis ) {
-        new_utxos.push( dummy_utxo )
-        dummy_utxo = utxo 
-      } else {
-        new_utxos.push( utxo )
-      }
-    }
-  }
-
-  const dummy_p2tr = payments.p2tr({
-    internalPubkey: toXOnly( Buffer.from( dummy_utxo.scriptPK ,'hex')),
-    network
-  })
+  if (dummy_utxos.length !== 2) throw new Error('No enough utxos.')
 
   // offer.input
+  console.log("Inputs: ", offer.data.inputs)  //1
+
+  const inscritpion_output = inscription_output.split(':')
+
+  const offer_sell_input = offer.data.inputs[1]
+
+  const buyer = payments.p2tr({
+    internalPubkey: toXOnly(Buffer.from(utxos[0].scriptPk, 'hex')),
+    network
+  })
+
+  const seller = payments.p2tr({
+    internalPubkey: toXOnly( offer_sell_input.witnessUtxo.script ),
+    network
+  })
+
+
+  const sell_input = {
+    hash: inscritpion_output[0],
+    index: +inscritpion_output[1],
+    witnessUtxo: offer_sell_input.witnessUtxo,
+    finalScriptWitness: offer_sell_input.finalScriptWitness,
+    tapInternalKey: seller.internalPubkey
+    
+  }
+  const sell_output = offer.txOutputs[1]
+
+  const psbt = new Psbt({
+    network
+  })
+
+  const dumy1 = dummy_utxos[0]
+  const dumy2 = dummy_utxos[1]
+
+  psbt.addInput({
+    hash: dumy1.txId,
+    index: +dumy1.outputIndex,
+    witnessUtxo: {
+      value: dumy1.satoshis,
+      script: buyer.output
+    },
+    tapInternalKey: buyer.internalPubkey,
+    sighashType: Transaction.SIGHASH_SINGLE | Transaction.SIGHASH_ANYONECANPAY,
+  })
+
+  psbt.addInput({
+    hash: dumy2.txId,
+    index: +dumy2.outputIndex,
+    witnessUtxo: {
+      value: dumy2.satoshis,
+      script: buyer.output
+    },
+    tapInternalKey: buyer.internalPubkey,
+    sighashType: Transaction.SIGHASH_SINGLE | Transaction.SIGHASH_ANYONECANPAY,
+  })
+
+  const dummy_amount = dumy1.satoshis + dumy2.satoshis 
+
+  // add signed inscription utxo
+  psbt.addInput( sell_input )
+
+  psbt.addOutput({
+    address: buyer.address,
+    value: dummy_amount
+  })
+
+  psbt.addOutput({
+    address: buyer.address,
+    value: sell_input.witnessUtxo.value
+  })
+
+  psbt.addOutput( sell_output )
+  
+  const psbt_size = 600
+  let buyer_cost = sell_input.witnessUtxo.value + fee_rate * psbt_size
+
+  while( buyer_cost > 0 ) {
+    const utxo = utxos.pop()
+    const { satoshis, txId, outputIndex } = utxo 
+    psbt.addInput({
+      hash: txId,
+      index: +outputIndex,
+      witnessUtxo: {
+        value: satoshis,
+        script: buyer.output
+      },
+      tapInternalKey: buyer.internalPubkey,
+      sighashType: Transaction.SIGHASH_SINGLE | Transaction.SIGHASH_ANYONECANPAY,
+    })
+
+    if( buyer_cost > satoshis ) {
+      buyer_cost = buyer_cost - satoshis
+    } else {
+      const left = satoshis - buyer_cost
+      if( left > 600 ) {
+        psbt.addOutput({
+          address: buyer.address,
+          value: left 
+        })
+      }
+    }
+
+    return psbt
+
+  }
+
+  // console.log(`Outpus: `, offer.txOutputs )
 
   // offer.updateInput( 0 , {
   //   hash: dummy_utxo.txId,
@@ -751,41 +865,33 @@ export async function createUnsignedBuyOffer({
 
   // })
 
-  return offer 
+  return offer
 
 }
 
 export async function createDummyUTXO({
   network,
-  utxo ,
+  utxos,
   fee_rate
-}:{
+}: {
   network: any;
-  utxo: UnspentOutput;
+  utxos: UnspentOutput[];
   fee_rate: number
 }) {
 
   const dummy_amount = 600
-  const network_fee = 154 * fee_rate
+  const network_fee = 174 * fee_rate
   const p2tr = payments.p2tr({
-    internalPubkey: toXOnly(Buffer.from( utxo.scriptPk, 'hex') ),
+    internalPubkey: toXOnly(Buffer.from(utxos[0].scriptPk, 'hex')),
     network
   })
 
   const dummy_utxo = new Psbt({
     network
   })
-  
-  dummy_utxo.addInput({
-    hash: utxo.txId,
-    index: +utxo.outputIndex,
-    witnessUtxo: {
-      value: utxo.satoshis,
-      script: p2tr.output
-    },
-    tapInternalKey: p2tr.internalPubkey,
-  })
-  
+
+  let need_sats = dummy_amount * 2 + network_fee
+
   dummy_utxo.addOutput({
     address: p2tr.address,
     value: dummy_amount
@@ -793,9 +899,63 @@ export async function createDummyUTXO({
 
   dummy_utxo.addOutput({
     address: p2tr.address,
-    value: utxo.satoshis - dummy_amount - network_fee
+    value: dummy_amount
   })
 
-  return dummy_utxo
+  while (need_sats > 0) {
+    const utxo = utxos.pop()
+    const value = utxo.satoshis
+    dummy_utxo.addInput({
+      hash: utxo.txId,
+      index: +utxo.outputIndex,
+      witnessUtxo: {
+        value: utxo.satoshis,
+        script: p2tr.output
+      },
+      tapInternalKey: p2tr.internalPubkey,
+    })
+    if (value >= need_sats) {
+      const left = value - need_sats
+      dummy_utxo.addOutput({
+        address: p2tr.address,
+        value: left
+      })
+    } else {
+      need_sats = need_sats - value
+    }
+  }
+
+  return {
+    dummy_utxo,
+    utxos
+  }
+
+}
+
+export async function selectDummyUtxo({
+  un_utxos,
+}: {
+  un_utxos: UnspentOutput[];
+}) {
+
+  const dummy_amount = 600 
+  const dummy_utxos = []
+  const utxos = []
+  
+  for(const utxo of un_utxos ) {
+    const { satoshis } = utxo 
+    if( dummy_utxos.length >= 2 ) {
+      utxos.push( utxo )
+    } else {
+      if( satoshis <= dummy_amount ) {
+        dummy_utxos.push( utxo )
+      }
+    }
+  }
+
+  return {
+    dummy_utxos ,
+    utxos
+  }
 
 }
--- test/makeoffer.test.ts
+++ test/makeoffer.test.ts
@@ -32,14 +32,15 @@ describe("Make Sell Offer ---->", () => {
                 value: 1234
             },
         })
-
+        
         console.log(offer.toHex())
 
     })
 
     it('Buy sign ----> ', async function () {
 
-        const unsigned_psbt_hex = `70736274ff0100dd0200000002da32f29e31b9c134432807bcf6a4ecd64fc17886f92609ec997b1b3ece0e50485902000000ffffffff6b8420b56db2e0975f568c6a94897d5a0eae5a2ab02e43777352435569045f930000000000ffffffff0322020000000000002251205416311c38701908e4f0ca5623bd631eda9b29c9923b7912031cf17b376aab623075000000000000225120dcb8e64c5ef2129668664066af9d403b9ad616d4eaa9381a75004fa7e73cd112d2040000000000002251202e3a8862b40e3a0b296c58fab3d16a35d980ce10bfec33539708fb10f37b01f3000000000001012b30750000000000002251205416311c38701908e4f0ca5623bd631eda9b29c9923b7912031cf17b376aab620117201bc91251f239f888706817e93e419f1532be84a1fc77166526902a36f6e8c7070001012b2202000000000000225120dcb8e64c5ef2129668664066af9d403b9ad616d4eaa9381a75004fa7e73cd1120108430141462faf9bf974b12def243e1b81e7c1ff7ee0ffecb28d56d24fc4beb85a65775b90f1252bd02e407fcb08cd6473f1f91e1138fcf4a46c2e03166e1f83ac7b6e778300000000`
+        const order_psbt_hex = `70736274ff0100dd0200000002da32f29e31b9c134432807bcf6a4ecd64fc17886f92609ec997b1b3ece0e50485902000000ffffffff6b8420b56db2e0975f568c6a94897d5a0eae5a2ab02e43777352435569045f930000000000ffffffff0322020000000000002251205416311c38701908e4f0ca5623bd631eda9b29c9923b7912031cf17b376aab623075000000000000225120dcb8e64c5ef2129668664066af9d403b9ad616d4eaa9381a75004fa7e73cd112d2040000000000002251202e3a8862b40e3a0b296c58fab3d16a35d980ce10bfec33539708fb10f37b01f3000000000001012b30750000000000002251205416311c38701908e4f0ca5623bd631eda9b29c9923b7912031cf17b376aab620117201bc91251f239f888706817e93e419f1532be84a1fc77166526902a36f6e8c7070001012b2202000000000000225120dcb8e64c5ef2129668664066af9d403b9ad616d4eaa9381a75004fa7e73cd1120108430141462faf9bf974b12def243e1b81e7c1ff7ee0ffecb28d56d24fc4beb85a65775b90f1252bd02e407fcb08cd6473f1f91e1138fcf4a46c2e03166e1f83ac7b6e778300000000`
+        const inscription_output = '935f04695543527377432eb02a5aae0e5a7d89946a8c565f97e0b26db520846bi0'
 
         const network = networks.testnet
         const amount = 30000
@@ -48,7 +49,21 @@ describe("Make Sell Offer ---->", () => {
 
         const to_submit_txs = []
 
-        const utxos = [{
+
+        const dummy_utxos = [{
+            txId: "935f04695543527377432eb02a5aae0e5a7d89946a8c565f97e0b26db520846b",
+            outputIndex: 0,
+            satoshis: 50000,
+            scriptPk: '03ac6d5bda18a62e940ee7e2c332da70968945a453329551001d5c8c6fe281669d',
+            addressType: AddressType.P2TR,
+            address: 'tb1pmjuwvnz77gffv6rxgpn2l82q8wddv9k5a25nsxn4qp860eeu6yfqeq3ssp',
+            inscriptions: [{
+                id: '',
+                offset: 0
+                // id: 'e6c0d0d309f94dd606cb81b02821e67da286156c8110fcaca4709f69fc42100ei0',
+                // offset: 0,
+            }]
+        },{
             txId: "935f04695543527377432eb02a5aae0e5a7d89946a8c565f97e0b26db520846b",
             outputIndex: 0,
             satoshis: 50000,
@@ -63,54 +78,71 @@ describe("Make Sell Offer ---->", () => {
             }]
         }]
 
-        if( utxos.length < 2 ) {
-            const dummy_utxo = await createDummyUTXO({
-                network,
-                utxo: utxos[0],
-                fee_rate
-            })
+        const utxos = [{
+            txId: "935f04695543527377432eb02a5aae0e5a7d89946a8c565f97e0b26db520846a",
+            outputIndex: 0,
+            satoshis: 50000,
+            scriptPk: '03ac6d5bda18a62e940ee7e2c332da70968945a453329551001d5c8c6fe281669d',
+            addressType: AddressType.P2TR,
+            address: 'tb1pmjuwvnz77gffv6rxgpn2l82q8wddv9k5a25nsxn4qp860eeu6yfqeq3ssp',
+            inscriptions: [{
+                id: '',
+                offset: 0
+                // id: 'e6c0d0d309f94dd606cb81b02821e67da286156c8110fcaca4709f69fc42100ei0',
+                // offset: 0,
+            }]
+        }]
 
-            // call unisat to sign ,https://demo.unisat.io/
-            // console.log( dummy_utxo.toHex())    
+        // if( utxos.length < 2 ) {
+        //     const dummy_utxo = await createDummyUTXO({
+        //         network,
+        //         utxo: utxos[0],
+        //         fee_rate
+        //     })
+
+        //     const scriptPk = utxos[0].scriptPk
+        //     // call unisat to sign ,https://demo.unisat.io/
+        //     // console.log( dummy_utxo.toHex())    
             
-            const signed_dummy_utxo = Psbt.fromHex('70736274ff01008902000000016b8420b56db2e0975f568c6a94897d5a0eae5a2ab02e43777352435569045f930000000000ffffffff025802000000000000225120dcb8e64c5ef2129668664066af9d403b9ad616d4eaa9381a75004fa7e73cd1125cbd000000000000225120dcb8e64c5ef2129668664066af9d403b9ad616d4eaa9381a75004fa7e73cd112000000000001012b50c3000000000000225120dcb8e64c5ef2129668664066af9d403b9ad616d4eaa9381a75004fa7e73cd11201084201409d8f8de53cfbe7ce298d46a5b70288fbfe2684bf3b4b89606ee987ab80c9478d9d0f44e32818ad13015fd298082de19f1efa4b84a8f8a4b39f1432eda8c5530a000000')
+        //     const signed_dummy_utxo = Psbt.fromHex('70736274ff01008902000000016b8420b56db2e0975f568c6a94897d5a0eae5a2ab02e43777352435569045f930000000000ffffffff025802000000000000225120dcb8e64c5ef2129668664066af9d403b9ad616d4eaa9381a75004fa7e73cd1125cbd000000000000225120dcb8e64c5ef2129668664066af9d403b9ad616d4eaa9381a75004fa7e73cd112000000000001012b50c3000000000000225120dcb8e64c5ef2129668664066af9d403b9ad616d4eaa9381a75004fa7e73cd11201084201409d8f8de53cfbe7ce298d46a5b70288fbfe2684bf3b4b89606ee987ab80c9478d9d0f44e32818ad13015fd298082de19f1efa4b84a8f8a4b39f1432eda8c5530a000000')
 
-            // to sign
-            const dummy_txid = signed_dummy_utxo.extractTransaction().getId()
+        //     // to sign
+        //     const dummy_txid = signed_dummy_utxo.extractTransaction().getId()
 
-            // // remove utxo
-            utxos.shift()
+        //     // // remove utxo
+        //     utxos.shift()
 
-            signed_dummy_utxo.txOutputs.forEach( ( output, idx ) => {
-                utxos.push({
-                    txId: dummy_txid,
-                    outputIndex: idx,
-                    satoshis: output.value,
-                    scriptPk: output.script.toString('hex'),
-                    addressType: AddressType.P2TR,
-                    address: output.address! ,
-                    inscriptions: [{
-                        id: '',
-                        offset: 0
-                        // id: 'e6c0d0d309f94dd606cb81b02821e67da286156c8110fcaca4709f69fc42100ei0',
-                        // offset: 0,
-                    }]
-                })
-            }) 
+        //     signed_dummy_utxo.txOutputs.forEach( ( output, idx ) => {
+        //         utxos.push({
+        //             txId: dummy_txid,
+        //             outputIndex: idx,
+        //             satoshis: output.value,
+        //             scriptPk ,
+        //             addressType: AddressType.P2TR,
+        //             address: output.address! ,
+        //             inscriptions: [{
+        //                 id: '',
+        //                 offset: 0
+        //                 // id: 'e6c0d0d309f94dd606cb81b02821e67da286156c8110fcaca4709f69fc42100ei0',
+        //                 // offset: 0,
+        //             }]
+        //         })
+        //     }) 
 
-        }
+        // }
 
-        console.log( utxos )
+        // console.log( utxos )
 
         const offer = await createUnsignedBuyOffer({
-            unsigned_psbt_hex,
+            order_psbt_hex,
             network,
+            dummy_utxos,
             utxos,
-            amount,
-            indexes
+            inscription_output,
+            fee_rate,
         })
 
-        // console.log(offer.txOutputs)
+        console.log(offer.txOutputs)
 
     })
 
